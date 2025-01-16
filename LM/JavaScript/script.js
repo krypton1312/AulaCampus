f15445867ad97a6966d8e7e0f6617c9ed46c2f50
Yehor Burlachenko <egor20042013@gmail.com>
2025-01-16 13:36:30 +0100

    lm

--- LM/JavaScript/script.js
+++ LM/JavaScript/script.js
@@ -1,6 +1,23 @@
-var titulo = document.getElementById("titulo");
+var caja = document.getElementById("caja");
+var boton = document.getElementById("boton");
+var boton1 = document.getElementById("boton1");
+var input = document.getElementById("input");
+var h1 = document.getElementById("h1");
+boton.style.display = 'absolute';
 
-titulo.style.color = "red";
-titulo.classList.add("verde");
-titulo.innerHTML += " opregkopeg";
-console.log();
+caja.addEventListener("click", function(){
+    caja.style.backgroundColor = "rgb(" + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + ")";
+});
+
+function EscribeTexto(){
+    h1.innerHTML = input.value;
+    caja.style.backgroundColor = "rgb(" + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + ")";
+}
+
+function Mover(){
+    boton.style.transform = "translate(" + Math.floor(Math.random() * 500) + "px," + Math.floor(Math.random() * 500) + "px)";
+}
+
+function changeColor(){
+    caja.style.backgroundColor = "rgb(" + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + "," + Math.floor(Math.random() *255) + ")";
+}
